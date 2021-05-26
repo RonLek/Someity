983c04fecbf4804547e93fdfcd36732eab7bad0d
Aaditya Joshi <38288671+aadityajo@users.noreply.github.com>
2021-05-26 21:26:01 +0530

    Revert "Merge pull request #10 from RonLek/revert-9-main"
    This reverts commit 2d2af311c0b1d8c3f5d3cdec0ad4e85b939d7538, reversing
    changes made to 5490e7685bd5c840cf086568d12f0a91f38e26a1.

--- options.js
+++ options.js
@@ -1,29 +1,31 @@
-chrome.storage.sync.set({
-	["assistant_enable"]: 1,
+chrome.storage.sync.get("assistant_enable", function (stored) {
+  if (stored.assistant_enable) {
+    $("#assistant_enable").prop("checked", true);
+  } else {
+    $("#assistant_enable").prop("checked", false);
+  }
 });
+
 var checkbox = document.querySelector("input[name=checkbox]");
-console.log(checkbox);
 checkbox.addEventListener("change", function () {
-	if (this.checked) {
-		chrome.storage.sync.set({
-			["assistant_enable"]: 1,
-		});
-	} else {
-		chrome.storage.sync.set({
-			["assistant_enable"]: 0,
-		});
-	}
+  if (this.checked) {
+    chrome.storage.sync.set({
+      ["assistant_enable"]: 1,
+    });
+  } else {
+    chrome.storage.sync.set({
+      ["assistant_enable"]: 0,
+    });
+  }
 });
 
 navigator.mediaDevices
-	.getUserMedia({ audio: true })
-	.then(function (stream) {
-		stream.getTracks().forEach(function (track) {
-			track.stop();
-		});
-
-		// close();
-	})
-	.catch(function (error) {
-		alert("Error : Microphone Access Required");
-	});
+  .getUserMedia({ audio: true })
+  .then(function (stream) {
+    stream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+  })
+  .catch(function (error) {
+    alert("Error : Microphone Access Required");
+  });
