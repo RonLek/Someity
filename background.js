cf3d7d42c48685400cc6538bf9ec22c8d1203bb0
Aaditya Joshi <38288671+aadityajo@users.noreply.github.com>
2021-05-26 21:26:10 +0530

    Revert "Revert "Merge pull request #10 from RonLek/revert-9-main""
    This reverts commit 983c04fecbf4804547e93fdfcd36732eab7bad0d.

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
-// On Install
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+// 	if (message.greeting == "removeCookie") {
+// 		sendResponse({ farewell: "cookie clean" });
+// 	}
+// });
+
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ ["assistant_enable"]: 1 });
   chrome.tabs.create(
     { url: `chrome-extension://${chrome.runtime.id}/options.html` },
-    function (tab) {}
+    function (tab) {
+      console.log("options page opened");
+    }
   );
   chrome.storage.sync.set({ ["clickedColor"]: "#3399FF80" });
-  chrome.storage.sync.set({ ["fontFamily"]: "open-dyslexic-regular" });
+  chrome.storage.sync.set({ ["fontFamily"]: "Arial" });
   chrome.storage.sync.set({ ["fontTypeButton"]: false });
   chrome.storage.sync.set({ ["fontSizeButton"]: false });
   chrome.storage.sync.set({ ["fontColorButton"]: false });
@@ -15,14 +21,17 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ ["magnifyButton"]: false });
   chrome.storage.sync.set({ ["imageVeilButton"]: false });
   chrome.storage.sync.set({ ["highlightWordsButton"]: false });
-  chrome.storage.sync.set({ ["emphasizeLinksButton"]: false });
-  chrome.storage.sync.set({ ["textStrokeButton"]: false });
-  chrome.storage.sync.set({ ["textStrokeColor"]: "#C0382B" });
-  chrome.storage.sync.set({ ["textStrokeColorId"]: "color-12" });
-  chrome.storage.sync.set({ ["scrollValue"]: 0 });
 });
 
-// On Tab Change
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+// 	// if (request.todo == "showPageAction") {
+// 	// 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+// 	// 		chrome.pageAction.show(tabs[0].id);
+// 	// 	});
+// 	// }
+// 	alert(request);
+// });
+
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.storage.sync.get(
     [
@@ -35,9 +44,6 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
       "magnifyButton",
       "highlightWordsButton",
       "imageVeilButton",
-      "emphasizeLinksButton",
-      "textStrokeButton",
-      "textStrokeColor",
     ],
     function (stored) {
       if (stored.fontTypeButton) {
@@ -53,6 +59,7 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
         });
       }
 
+      console.log("Stored = ", stored);
       if (stored.fontSizeButton) {
         chrome.tabs.sendMessage(activeInfo.tabId, {
           todo: "fontSize",
@@ -93,24 +100,6 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
         todo: "highlight",
         checkedButton: stored.highlightWordsButton ? 1 : 0,
       });
-
-      chrome.tabs.sendMessage(activeInfo.tabId, {
-        todo: "emphasizeLinks",
-        checkedButton: stored.emphasizeLinksButton ? 1 : 0,
-      });
-
-      if (stored.textStrokeButton) {
-        chrome.tabs.sendMessage(activeInfo.tabId, {
-          todo: "textStroke",
-          textStrokeColor: stored.textStrokeColor,
-          checkedButton: 1,
-        });
-      } else {
-        chrome.tabs.sendMessage(activeInfo.tabId, {
-          todo: "textStroke",
-          checkedButton: 0,
-        });
-      }
     }
   );
 });
@@ -122,6 +111,7 @@ chrome.runtime.onConnect.addListener(function (port) {
     if (msg.action == "open") {
       temp = msg.result;
       if (temp != null && temp !== "undefined") {
+        console.log(temp);
         chrome.tabs.create({
           url: temp,
         });
@@ -131,6 +121,7 @@ chrome.runtime.onConnect.addListener(function (port) {
       temp = msg.result;
 
       if (temp != null && temp !== "undefined") {
+        console.log(temp);
         chrome.tabs.create({
           url: temp,
         });
@@ -139,6 +130,7 @@ chrome.runtime.onConnect.addListener(function (port) {
     } else if (msg.action == "translate") {
       temp = msg.result;
       if (temp != null && temp !== "undefined") {
+        console.log(temp);
         chrome.tabs.create({
           url: temp,
         });
@@ -147,6 +139,7 @@ chrome.runtime.onConnect.addListener(function (port) {
     } else if (msg.action == "direction") {
       temp = msg.result;
       if (temp != null && temp !== "undefined") {
+        console.log(temp);
         chrome.tabs.create({
           url: temp,
         });
@@ -156,7 +149,6 @@ chrome.runtime.onConnect.addListener(function (port) {
   });
 });
 
-// Context Menu
 var menuItem = {
   id: "Speak",
   title: "Speak",
@@ -174,18 +166,4 @@ chrome.contextMenus.onClicked.addListener(function (clickData, tabdata) {
   }
 });
 
-// Chrome Screenshot
-var id = 100;
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.todo == "screenshot") {
-    chrome.tabs.captureVisibleTab({ format: "png" }, function (screenshotUrl) {
-      chrome.storage.local.set({ ["setScreenshot"]: screenshotUrl });
-
-      var viewTabUrl = chrome.runtime.getURL("screenshot.html?id=" + id);
-      var targetId = null;
-      chrome.tabs.create({ url: viewTabUrl }, function (tab) {
-        targetId = tab.id;
-      });
-    });
-  }
-});
+// Chrome on startup
